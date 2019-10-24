0f83ca7333dc8d6e942e0421e7aaa51b3859473e
100134969 <kingpatrick455@gmail.com>
2019-10-24 08:34:54 -0500

    concatenation and repetition

---
+++ stringsAndThings.py
@@ -0,0 +1,25 @@
+# strings
+#  data that falls within" " marks
+
+# Concatenation
+# Put 2 or more strings together
+
+firstname = "Fred"
+lastname = "Flintstone"
+
+fullname = firstname + " " + lastname
+
+print(fullname)
+
+# Repetition
+# repetition operator: *
+
+print("Hip "*2+ "Hooray!")
+
+def rowyourboat():
+    print("Row, "*3 + 'your boat')
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowyourboat()
